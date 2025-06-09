9d66fa5370aaf08b9bcf15e32a8aff8889671158
Ganga394 <sgangadevi006@gmail.com>
2025-06-09 19:20:39 +0530

    Create Address-book.java

---
+++ Sat Sun-Tasks/Address-book.java
@@ -0,0 +1,28 @@
+//address book
+import java.util.Scanner;
+class main {
+	public static void main(String[] args) {
+	    Scanner s=new Scanner(System.in);
+	    int size=3;
+	    String[] name=new String[size];
+	    String[] num=new String[size];
+      String[] num=new String[size];
+	    System.out.println("Enter 3 info(Name, Phone Number, and Email):");
+	    for(int i=0;i<size;i++){
+	        System.out.println("Enter name"+(i+1)+":");
+	        name[i]=s.nextLine();
+	        System.out.println("Enter number"+(i+1)+":");
+	        num[i]=s.nextLine();
+          System.out.println("Enter email"+(i+1)+":");
+          email[i]=s.nextLine();
+ }
+	    System.out.println("ADDRESS BOOK");
+	    System.out.println("-------------------");
+	    for(int i=0;i<size;i++){
+	        System.out.println("Name:"+name[i]);
+	        System.out.println("Phone:"+num[i]);
+          System.out.println("Email:"+email[i]);
+	        System.out.println("-------------------");
+	    }
+	}
+	}
