e368c9401b564a0b614022e109e7d6c1fddd168b
Ganga394 <sgangadevi006@gmail.com>
2025-06-10 09:36:52 +0530

    Update Task2.java

--- DAY3/Task2.java
+++ DAY3/Task2.java
@@ -1,58 +1,54 @@
-abstract class Employee {
+package Inheritances;
+
+class Person {
     String name;
-    Employee(String name) { this.name = name; }
-    abstract double calculateSalary();
-    void displaySalary() {
-        System.out.println(name + "s Salary: $" + calculateSalary());
+
+    Person(String name) {
+        this.name = name;
+    }
+}
+
+class Employee extends Person {
+    double salary;
+
+    Employee(String name) {
+        super(name);
+    }
+
+    void showSalary() {
+        System.out.println(name + "'s Salary: $" + salary);
     }
 }
 
 class FullTimeEmployee extends Employee {
-    double salary;
-    FullTimeEmployee(String name, double salary) {
-    super(name); 
-    this.salary = salary;
-    }
-    double calculateSalary() { 
-    return salary; 
+    FullTimeEmployee(String name, double fixedSalary) {
+        super(name);
+        this.salary = fixedSalary;
     }
 }
 
 class PartTimeEmployee extends Employee {
-    int hours; 
-    double rate;
     PartTimeEmployee(String name, int hours, double rate) {
-    super(name); 
-    this.hours = hours; 
-    this.rate = rate;
-    }
-    double calculateSalary() { 
-    return hours * rate;
+        super(name);
+        this.salary = hours * rate;
     }
 }
 
 class Freelancer extends Employee {
-    int hours; double rate, bonus;
     Freelancer(String name, int hours, double rate, double bonus) {
-    super(name); 
-    this.hours = hours; 
-    this.rate = rate; 
-    this.bonus = bonus;
-    }
-    double calculateSalary() {
-    return hours * rate + bonus; 
+        super(name);
+        this.salary = (hours * rate) + bonus;
     }
 }
 
-public class SalaryCalculator {
+public class multilevel {
     public static void main(String[] args) {
-        Employee[] employees = {
-            new FullTimeEmployee("Reshma", 30000),
-            new PartTimeEmployee("John", 80, 20),
-            new Freelancer("Renu", 100, 25, 500)
-        };
+        FullTimeEmployee e1 = new FullTimeEmployee("Abi", 30000);
+        PartTimeEmployee e2 = new PartTimeEmployee("Renu", 80, 20);
+        Freelancer e3 = new Freelancer("Chotu", 60, 25, 300);
 
-        for (Employee e : employees) 
-        e.displaySalary();
+        e1.showSalary();
+        e2.showSalary();
+        e3.showSalary();
     }
 }
