ffcb4f2952c3b7928da8683101f956564a774a25
Ganga394 <sgangadevi006@gmail.com>
2025-06-19 14:43:46 +0530

    Update and rename Task1.sql to Task1,2.sql

--- Day 11/Task1,2.sql
+++ Day 11/Task1,2.sql
@@ -27,6 +27,20 @@ VALUES
 (105, 3, 'Ssos', 95),
 (106, 3, 'Oracle', 88);
 
+--Join students and marks
 SELECT s.student_id, s.name, s.class, m.subject, m.marks
 FROM Students s
 JOIN Marks m ON s.student_id = m.student_id;
+
+--sub-query to find top performer
+SELECT student_id, name, total_marks
+FROM (
+    SELECT s.student_id, s.name, SUM(m.marks) AS total_marks
+    FROM Students s
+    JOIN Marks m ON s.student_id = m.student_id
+    GROUP BY s.student_id, s.name
+) AS student_totals
+ORDER BY total_marks DESC
+LIMIT 1;
+
+
